da927788eee7720e8d1abfd7761e5bb76de77972
Alexlife9 <crashmx@inbox.ru>
2022-09-12 18:55:54 +0300

    Синхронизация общих ресурсов

---
+++ src/main/java/ru/job4j/thread/Cache.java
@@ -0,0 +1,19 @@
+package ru.job4j.thread;
+
+/**
+ * Синхронизация общих ресурсов
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 12.09.2022
+ */
+public final class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
