4daee08560176cbafe4565cadb1d7cd739279120
Alexlife9 <crashmx@inbox.ru>
2022-09-13 15:28:55 +0300

    Immutable объекты

---
+++ src/main/java/ru/job4j/thread/Node.java
@@ -0,0 +1,41 @@
+package ru.job4j.thread;
+
+/**
+ * Immutable объекты
+ *
+ * Проблемы в многопоточной среде возникают из-за объектов, которые могут менять состояния.
+ * Правила создания Immutable объекта.
+ * 1. Все поля отмечены final.
+ * 2. Состояние объекта не изменяется после создания объекта.
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 13.09.2022
+ */
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    /* поскольку поля final - установить новые значения мы не сможем, поэтому сеттеры не нужны
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+     */
+}
